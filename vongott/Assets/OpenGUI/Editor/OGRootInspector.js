ec7638ec9f4487e15068534e580501621e52ef59
unknown <jeppen@jampe.dk>
2014-01-23 14:49:33 +0800

    opengui upgrade

--- vongott/Assets/OpenGUI/Editor/OGRootInspector.js
+++ vongott/Assets/OpenGUI/Editor/OGRootInspector.js
@@ -1,10 +1,23 @@
 #pragma strict
 
 @CustomEditor ( OGRoot )
+@InitializeOnLoad
 public class OGRootInspector extends Editor {
+	private static function OGRootInspector () {
+		OGRoot.EditorSelectWidget = function ( w : OGWidget ) {
+			Selection.activeObject = w.gameObject;
+		};
+	}
+	
 	override function OnInspectorGUI () {
 		var root : OGRoot = target as OGRoot;
-
+	
+		if ( OGRoot.EditorSelectWidget == null ) {
+			OGRoot.EditorSelectWidget = function ( w : OGWidget ) {
+				Selection.activeObject = w.gameObject;
+			};
+		}
+		
 		if ( !root ) { return; }
 		
 		DrawDefaultInspector ();
