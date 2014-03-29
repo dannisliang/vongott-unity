cfdc47d9487e8392ac5a0430c2b0ba0f8b028f77
yipyip <contact@jeppezapp.com>
2014-03-29 16:24:31 +0800

    implementing developer console and improved background music management

--- vongott/Assets/Scripts/Core/Tools/Console.js
+++ vongott/Assets/Scripts/Core/Tools/Console.js
@@ -16,7 +16,7 @@ public class Console extends MonoBehaviour {
 
 		if ( args.Length > 1 ) {
 			switch ( args[0] ) {
-				case "music":
+				case "soundtrack":
 					switch ( args[1] ) {
 						case "aggressive":
 							MusicManager.GetInstance().PlayAggressive();
@@ -29,25 +29,85 @@ public class Console extends MonoBehaviour {
 							break;
 
 						default:
-							output = "No soundtrack for the state '" + args[1] + "'";
+							output = "[ERROR] No soundtrack for the state '" + args[1] + "'";
 							break;
 					}
 					break;
 
+				case "player":
+					switch ( args[1] ) {
+						case "immortal":
+							// TODO: god mode on
+							output =  "Player is immortal";
+							break;
+
+						case "mortal":
+							// TODO: god mode off
+							output = "Player is mortal";
+							break;
+
+						default:
+							output = "[ERROR] No player state '" + args[1] + "'";
+							break;
+					}
+					
+					break;
+
 				default:
-					output = "ERROR: set: Invalid argument '" + args[0] + "'";
+					output = "[ERROR] set: Invalid argument '" + args[0] + "'";
 					break;			
 							
 			}
 		} else {
-			output = "ERROR: set: Not enough arguments provided. Input was '" + input + "'";
+			output = "[ERROR] set: Not enough arguments provided. Input was '" + input + "'";
 
 		}
 
 		return output;
 	}
 
+	private function Give ( input : String ) : String {
+		var args : String [] = input.Split ( " "[0] );
+		var output : String = "";
+
+		if ( args.Length > 1 ) {
+			switch ( args[0] ) {
+				case "weapon":
+					switch ( args[1] ) {
+						case "all":
+							// TODO: give all weapons
+							output =  "Got all weapons";
+							break;
+
+						default:
+							output = "[ERROR] No weapon by the name '" + args[1] + "'";
+							break;
+					}
+					break;
+
+				default:
+					output = "[ERROR] give: Invalid argument '" + args[0] + "'";
+					break;			
+							
+			}
+		} else {
+			output = "[ERROR] give: Not enough arguments provided. Input was '" + input + "'";
+
+		}
+		
+
+		return output;
+	}
+
 	public function Parse ( input : String ) : String {
+		// Shorcuts
+		if ( input == "god" ) {
+			// TODO: check for toggle
+			return Set ( "player immortal" );	
+		} else if ( input == "allweapons" ) {
+			return Give ( "weapon all" );
+		}
+		
 		var strings : String [] = input.Split ( " "[0], 2 );
 		var output : String = "";
 
@@ -64,6 +124,11 @@ public class Console extends MonoBehaviour {
 					output = Set ( args );
 					break;
 			
+				// Give
+				case "give":
+					output = Give ( args );
+					break;
+
 				// Unknown
 				default:
 					output = "ERROR: Invalid function '" + strings[0] + "'";
