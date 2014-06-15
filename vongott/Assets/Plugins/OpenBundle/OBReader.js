85801a8e52005461cc6748349e05e02980904245
yipyip <contact@jeppezapp.com>
2014-06-15 13:33:46 +0800

    starting openbundle

---
+++ vongott/Assets/Plugins/OpenBundle/OBReader.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+import System.Text;
+import System.IO;
+
+public class OBManager {
+	public function LoadBundle ( path : String ) : OBBundle {
+		path = path.Replace ( "\\", "/" );
+		var strings : String = path.Split ( "/"[0] );
+		var name : String = strings [ strings.Length - 1 ];
+
+		
+	}
+}
