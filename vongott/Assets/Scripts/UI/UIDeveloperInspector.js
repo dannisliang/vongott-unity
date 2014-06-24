28c569c55d4e59d9e21a4ab09895ca8c3fb37c94
unknown <jeppen@jampe.dk>
2014-06-25 02:50:19 +0800

    starting new ui skin

---
+++ vongott/Assets/Scripts/UI/UIDeveloperInspector.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+public class UIDeveloperInspector extends OEComponentInspector {
+	public enum Mode { 
+		Flags,
+		Quests
+	}
+
+	public var core : GameCore;
+	
+	private var mode : Mode;
+
+	override function get type () : System.Type { return typeof ( GameCore ); }
+	
+	override function Inspector () {
+		if ( !core ) { return; }	
+	
+		width = 380;
+		mode = Popup ( "Mode", mode, System.Enum.GetNames ( Mode ) );
+
+		switch ( mode ) {
+			case Mode.Flags:
+				
+				break;
+
+			case Mode.Quests:
+				var questManager : OCQuests = GameCore.GetQuestManager ();
+
+				for ( var q : OCQuests.Quest in questManager.userQuests ) {
+					LabelField ( q.title );
+
+					offset.x += 20;
+
+					for ( var o : OCQuests.Objective in q.objectives ) {
+						var desc : String = o.description;
+
+						if ( desc.Length > 20 ) {
+							desc = desc.Substring ( 0, 20 ) + "...";
+						}
+
+						o.progress = Slider ( desc, o.progress, 0, o.goal );
+					}
+					
+					offset.x -= 20;
+					
+					offset.y += 20;
+				}
+
+				break;
+		}
+	}	
+}
