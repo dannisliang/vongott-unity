147ffd5b837d1434298c798ab0f714d3100213f4
yipyip <contact@jeppezapp.com>
2014-09-03 18:56:23 +0800

    camera editor

--- vongott/Assets/Editor/OpenJib/OJSequenceInspector.js
+++ vongott/Assets/Editor/OpenJib/OJSequenceInspector.js
@@ -2,78 +2,33 @@
 
 import DG.Tweening;
 
-@CustomEditor ( OJManager )
-public class OJManagerInspector extends Editor {
+@CustomEditor ( OJSequence )
+public class OJSequenceInspector extends Editor {
 	private static var currentSequence : int;
 	private static var currentKeyframe : int;
 	
 	override function OnInspectorGUI () {
-		var manager : OJManager = target as OJManager;
+		var sequence : OJSequence = target as OJSequence;
 	
-		if ( manager.sequences.Count < 1 ) {
-			manager.sequences.Add ( new OJSequence () );
-		}
+		sequence.cam = EditorGUILayout.ObjectField ( "Camera", sequence.cam, typeof ( Camera ), true ) as Camera;
 
-		EditorGUILayout.BeginHorizontal ();
-
-		// Pick sequence
-		var sequenceNames : List.< String > = new List.< String > ();
-
-		for ( var s : OJSequence in manager.sequences ) {
-			sequenceNames.Add ( s.name );
-		}
-
-		currentSequence = EditorGUILayout.Popup ( currentSequence, sequenceNames.ToArray() );
-
-		if ( currentSequence >= manager.sequences.Count ) {
-			currentSequence = manager.sequences.Count - 1;
-
-		}
-
-		// Remove sequence
-		GUI.backgroundColor = Color.red;
-		if ( GUILayout.Button ( "-", GUILayout.Width ( 24 ), GUILayout.Height ( 16 ) ) ) {
-			manager.sequences.RemoveAt ( currentSequence );
+		if ( !sequence.cam ) {
+			EditorGUILayout.LabelField ( "Please link a camera first" );
 			return;
 		}
 
-		// Add sequence
-		GUI.backgroundColor = Color.green;
-		if ( GUILayout.Button ( "+", GUILayout.Width ( 24 ), GUILayout.Height ( 16 ) ) ) {
-			manager.sequences.Add ( new OJSequence () );
-			currentSequence = manager.sequences.Count - 1;
-			return;
-		}
-		
-		GUI.backgroundColor = Color.white;
-		
-		// Sort sequences
-		if ( GUILayout.Button ( "Sort" ) ) {
-			manager.SortSequences ();
-		}
-		
-		EditorGUILayout.EndHorizontal ();
-
-		EditorGUILayout.Space ();
-		var seq : OJSequence = manager.sequences [ currentSequence ];
-		
-		// Sequence properties
-		seq.name = EditorGUILayout.TextField ( "Name", seq.name );
-
-		EditorGUILayout.Space ();
-	
 		EditorGUILayout.BeginHorizontal ();
 		
 		if ( GUILayout.Button ( "PLAY" ) ) {
-			manager.Play ( currentSequence );
+			sequence.Play ();
 		}
 		
 		if ( GUILayout.Button ( "PAUSE" ) ) {
-			manager.Stop ();
+			sequence.Stop ();
 		}
 		
 		if ( GUILayout.Button ( "RESET" ) ) {
-			manager.Reset ();
+			sequence.Reset ();
 		}
 
 		EditorGUILayout.EndHorizontal ();
@@ -81,21 +36,21 @@ public class OJManagerInspector extends Editor {
 		EditorGUILayout.Space ();
 
 		// Pick keyframe
-		EditorGUILayout.LabelField ( "Keyframe: " + manager.currentKeyframe + " / " + ( seq.keyframes.Count - 1 ), EditorStyles.boldLabel );
+		EditorGUILayout.LabelField ( "Keyframe: " + sequence.currentKeyframe + " / " + ( sequence.keyframes.Count - 1 ), EditorStyles.boldLabel );
 		
 		GUILayout.Space ( 30 );
 		
 		EditorGUILayout.BeginHorizontal ();
 		
-		if ( seq.keyframes.Count < 1 ) {
-			seq.keyframes.Add ( new OJKeyframe () );
+		if ( sequence.keyframes.Count < 1 ) {
+			sequence.keyframes.Add ( new OJKeyframe () );
 		}
 
-		if ( currentKeyframe >= seq.keyframes.Count ) {
-			currentKeyframe = seq.keyframes.Count - 1;
+		if ( currentKeyframe >= sequence.keyframes.Count ) {
+			currentKeyframe = sequence.keyframes.Count - 1;
 		}
 
-		for ( var i : int = 0; i < seq.keyframes.Count; i++ ) {
+		for ( var i : int = 0; i < sequence.keyframes.Count; i++ ) {
 			if ( i == currentKeyframe ) {
 				GUI.backgroundColor = Color.white;
 			} else {
@@ -104,7 +59,7 @@ public class OJManagerInspector extends Editor {
 
 			if ( GUILayout.Button ( i.ToString(), GUILayout.Width ( 24 ), GUILayout.Height ( 16 ) ) ) {
 				currentKeyframe = i;
-				manager.SetKeyframePose ( i );
+				sequence.SetKeyframePose ( i );
 			}
 			
 			if ( i == currentKeyframe ) {
@@ -115,7 +70,7 @@ public class OJManagerInspector extends Editor {
 
 				GUI.backgroundColor = Color.red;
 				if ( GUI.Button ( bRect, "-" ) ) {
-					seq.keyframes.RemoveAt ( i );
+					sequence.keyframes.RemoveAt ( i );
 					return;
 				}
 				GUI.backgroundColor = Color.white;
@@ -125,27 +80,27 @@ public class OJManagerInspector extends Editor {
 					bRect.width = 16;
 
 					if ( GUI.Button ( bRect, "<" ) ) {
-						var a : OJKeyframe = seq.keyframes [ i - 1 ];
-						var b : OJKeyframe = seq.keyframes [ i ];
+						var a : OJKeyframe = sequence.keyframes [ i - 1 ];
+						var b : OJKeyframe = sequence.keyframes [ i ];
 
-						seq.keyframes [ i - 1 ] = b;
-						seq.keyframes [ i ] = a;
+						sequence.keyframes [ i - 1 ] = b;
+						sequence.keyframes [ i ] = a;
 
 						currentKeyframe--;
 						return;
 					}
 				}
 				
-				if ( i < seq.keyframes.Count - 1 ) {
+				if ( i < sequence.keyframes.Count - 1 ) {
 					bRect.x = rect.xMax + 4;
 					bRect.width = 16;
 
 					if ( GUI.Button ( bRect, ">" ) ) {
-						a = seq.keyframes [ i + 1 ];
-						b = seq.keyframes [ i ];
+						a = sequence.keyframes [ i + 1 ];
+						b = sequence.keyframes [ i ];
 
-						seq.keyframes [ i + 1 ] = b;
-						seq.keyframes [ i ] = a;
+						sequence.keyframes [ i + 1 ] = b;
+						sequence.keyframes [ i ] = a;
 
 						currentKeyframe++;
 						return;
@@ -158,7 +113,7 @@ public class OJManagerInspector extends Editor {
 		
 		GUI.backgroundColor = Color.green;
 		if ( GUILayout.Button ( "+", GUILayout.Width ( 24 ), GUILayout.Height ( 16 ) ) ) {
-			seq.keyframes.Add ( new OJKeyframe () );
+			sequence.keyframes.Add ( new OJKeyframe () );
 		}
 
 		GUI.backgroundColor = Color.white;
@@ -167,15 +122,15 @@ public class OJManagerInspector extends Editor {
 
 		EditorGUILayout.Space ();
 
-		var kf : OJKeyframe = seq.keyframes [ currentKeyframe ];
+		var kf : OJKeyframe = sequence.keyframes [ currentKeyframe ];
 		
 		// Transform
 		EditorGUILayout.BeginHorizontal ();
 		EditorGUILayout.LabelField ( "Transform", EditorStyles.boldLabel );
 
 		if ( GUILayout.Button ( "Copy from scene" ) ) {
-			kf.position = manager.transform.position;
-			kf.rotation = manager.transform.localEulerAngles;
+			kf.position = sequence.cam.transform.position;
+			kf.rotation = sequence.cam.transform.localEulerAngles;
 		}
 		EditorGUILayout.EndHorizontal ();
 		
--- vongott/Assets/Plugins/OpenJib/OJSequence.js
+++ vongott/Assets/Plugins/OpenJib/OJSequence.js
@@ -23,11 +23,96 @@ public class OJKeyframe {
 	}
 }
 
-public class OJSequence {
-	public var name : String = "New Sequence";
+public class OJSequence extends MonoBehaviour {
 	public var keyframes : List.< OJKeyframe > = new List.< OJKeyframe > (); 
+	public var cam : Camera;
 
-	function OJSequence () {
-		keyframes.Add ( new OJKeyframe () );
+	@HideInInspector public var currentKeyframe : int;
+
+	private function get isReady () : boolean {
+		return Application.isPlaying && cam != null && keyframes.Count > 0 && currentKeyframe < keyframes.Count;
+	}
+	
+	public function PlayNextKeyframe () {
+		currentKeyframe++;
+
+		if ( !keyframes [ currentKeyframe ].stop ) {
+			PlayCurrentKeyframe ();
+		}
+	}
+
+	// Frame functions
+	public function SetKeyframePose ( i : int ) {
+		var kf : OJKeyframe = keyframes [ i ];
+		
+		SetPosition ( kf.position );
+		SetRotation ( kf.rotation );
+	}
+
+	// Tween functions
+	public function SetPosition ( value : Vector3 ) {
+		cam.transform.position = value;
+	}
+
+	public function GetPosition () : Vector3 {
+		return cam.transform.position;
+	}
+
+	public function SetRotation ( value : Vector3 ) {
+		cam.transform.localRotation = Quaternion.Euler ( value );
+	}
+
+	public function GetRotation () : Vector3 {
+		return cam.transform.localEulerAngles;
+	}
+
+	public function PlayCurrentKeyframe () {
+		if ( isReady && currentKeyframe < keyframes.Count - 1 ) {
+			var thiskf : OJKeyframe = keyframes [ currentKeyframe ];
+			var nextkf : OJKeyframe = keyframes [ currentKeyframe + 1 ];
+
+			var moveTween : Tween = DOTween.To (
+				GetPosition,
+				SetPosition,
+				nextkf.position,
+				thiskf.time
+			);
+			
+			var rotateTween : Tween = DOTween.To (
+				GetRotation,
+				SetRotation,
+				nextkf.rotation,
+				thiskf.time
+			);
+
+			rotateTween.SetEase ( thiskf.easing );
+			moveTween.SetEase ( thiskf.easing );
+		}
+	}
+
+	public function Play () {
+		if ( isReady ) {
+			cam.enabled = true;
+			PlayCurrentKeyframe ();
+		}
+	}
+
+	public function Reset () {
+		currentKeyframe = 0;
+
+		SetPosition ( keyframes [ currentKeyframe ].position );
+		SetRotation ( keyframes [ currentKeyframe ].rotation );
+	}
+
+	public function Start () {
+		cam.enabled = false;
+	}
+
+	public function Stop () {
+	
+	}
+
+	public function Exit () {
+		cam.enabled = false;
 	}
 }
